370fd484d70ab91ecf2c75a75a92576fca9832fb
jordanbenbelaid <jordanbenbelaid@hotmail.com>
2021-02-22 11:09:02 +0000

    orders updated with orderline features

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -4,19 +4,24 @@ public class Order {
 
 		private Long id;
 		private Long custId;
+		private Customer customer;
+		private Item item;
 		private Long orderLineId;
 		
-		public Order(Long custId, Long orderLineId) {
+		public Order(Long custId, Long orderLineId, Customer customer, Item item) {
 			this.setCustId(custId);
 			this.setOrderLineId(orderLineId);
+			this.setCustomer(customer);
+			this.setItem(item);
 		}
 
-		public Order(Long id, Long custId, Long orderLineId) {
+		public Order(Long id, Long custId, Long orderLineId, Customer customer, Item item) {
 			this.setId(id);
 			this.setCustId(custId);
 			this.setOrderLineId(orderLineId);
+			this.setCustomer(customer);
+			this.setItem(item);
 		}
-		
 
 		public Long getId() {
 			return id;
@@ -34,6 +39,22 @@ public class Order {
 			this.custId = custId;
 		}
 
+		public Customer getCustomer() {
+			return customer;
+		}
+
+		public void setCustomer(Customer customer) {
+			this.customer = customer;
+		}
+
+		public Item getItem() {
+			return item;
+		}
+
+		public void setItem(Item item) {
+			this.item = item;
+		}
+
 		public Long getOrderLineId() {
 			return orderLineId;
 		}
@@ -47,7 +68,9 @@ public class Order {
 			final int prime = 31;
 			int result = 1;
 			result = prime * result + ((custId == null) ? 0 : custId.hashCode());
+			result = prime * result + ((customer == null) ? 0 : customer.hashCode());
 			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			result = prime * result + ((item == null) ? 0 : item.hashCode());
 			result = prime * result + ((orderLineId == null) ? 0 : orderLineId.hashCode());
 			return result;
 		}
@@ -66,10 +89,20 @@ public class Order {
 					return false;
 			} else if (!custId.equals(other.custId))
 				return false;
+			if (customer == null) {
+				if (other.customer != null)
+					return false;
+			} else if (!customer.equals(other.customer))
+				return false;
 			if (id == null) {
 				if (other.id != null)
 					return false;
 			} else if (!id.equals(other.id))
+				return false;
+			if (item == null) {
+				if (other.item != null)
+					return false;
+			} else if (!item.equals(other.item))
 				return false;
 			if (orderLineId == null) {
 				if (other.orderLineId != null)
@@ -78,4 +111,6 @@ public class Order {
 				return false;
 			return true;
 		}
+		
+		
 }
